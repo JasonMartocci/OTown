65fe4e56e3e189cdbfaf327fb5d09d9fe1d31298
Jason Martocci <martoccijason@gmail.com>
2016-06-13 11:58:26 -0400

    O Town

---
+++ assets/javascript/chat.js
@@ -0,0 +1,25 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+     // Chat feature
+    var myDataRef = new Firebase('https://vivid-torch-7282.firebaseio.com/chat');
+
+    // Chat box add data on keypress
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').addClass('chatMsg').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      }; 
+});
